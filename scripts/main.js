87946c8a4af31777b264e96b5a09fd80ee1b887d
Axel <axelviskovic@gmail.com>
2019-10-08 10:18:41 +0200

    test
    test

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,11 @@ const imgPlayer = document.querySelector("#playerIMG")
 const imgNerve = document.querySelector("#nerveIMG")
 const txtChoisisRole = document.querySelector(".containerTxt h3")
 
+let choiceRole=0
+// 1 = Watcher
+// 2 = Player
+
+
 imgWatcher.addEventListener(
   "mouseover",
   function(){
@@ -41,15 +46,16 @@ imgWatcher.addEventListener(
     imgPlayer.style.transform="translateX(100%)"
     imgPlayer.style.opacity="0"
     imgPlayer.style.transition="all 1s"
-    imgNerve.style.transform="translateY(-100%)"
+    imgNerve.style.transform="translateX(100%)"
     imgNerve.style.opacity="0"
     imgNerve.style.transition="all 1s"
     imgWatcher.style.width="600px"
-    imgWatcher.style.transform="translateX(30%)"
+    imgWatcher.style.transform="translateY(-250%)translateX(30%)"
     imgWatcher.style.opacity="1"
     imgWatcher.style.transition="all 1s"
     txtChoisisRole.style.opacity="0"
     txtChoisisRole.style.transition="all 1s"
+    choiceRole = 1
   }
 )
 
@@ -59,14 +65,23 @@ imgPlayer.addEventListener(
     imgWatcher.style.transform="translateX(-100%)"
     imgWatcher.style.opacity="0"
     imgWatcher.style.transition="all 1s"
-    imgNerve.style.transform="translateY(-100%)"
+    imgNerve.style.transform="translateX(-100%)"
     imgNerve.style.opacity="0"
     imgNerve.style.transition="all 1s"
     imgPlayer.style.width="600px"
-    imgPlayer.style.transform="translateX(-30%)"
+    imgPlayer.style.transform="translateX(-30%)translateY(-250%)"
     imgPlayer.style.opacity="1"
     imgPlayer.style.transition="all 1s"
     txtChoisisRole.style.opacity="0"
     txtChoisisRole.style.transition="all 1s"
+    choiceRole = 2
   }
 )
+
+if(choiceRole==1){
+  console.log("wsh")
+  window.alert("watcher")
+}
+if(choiceRole==2){
+  window.alert("player")
+}
